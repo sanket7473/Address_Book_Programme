75bdea638e785686580901045ce2a1c782fd12fe
Sanket Garde <sanketgarde7473@gmail.com>
2023-11-21 11:47:22 +0530

    Added:Java programme for Address Book for the Welcome Message

---
+++ src/AddressBookMain.java
@@ -0,0 +1,12 @@
+public class AddressBookMain {
+
+    public void print_msg()
+    {
+        System.out.println("Welcome to Address Book Programme");
+    }
+    public static void main(String[] args)
+    {
+       AddressBookMain s1= new AddressBookMain();
+       s1.print_msg();
+    }
+}
