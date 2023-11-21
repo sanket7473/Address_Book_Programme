b12267d2eb4af76924196cac5ddb7c418355a5d3
Sanket Garde <sanketgarde7473@gmail.com>
2023-11-21 16:01:41 +0530

    Added:Java programme for Address Book to create a contacts in address book

--- src/com/address_book/AddressBookMain.java
+++ src/com/address_book/AddressBookMain.java
@@ -1,12 +1,13 @@
-public class AddressBookMain {
+package com.address_book;
 
+public class AddressBookMain {
     public void print_msg()
     {
         System.out.println("Welcome to Address Book Programme");
     }
     public static void main(String[] args)
     {
-       AddressBookMain s1= new AddressBookMain();
-       s1.print_msg();
+        AddressBookMain s1= new AddressBookMain();
+        s1.print_msg();
     }
 }
